a816bebacb4a9e72b92db53a1c312b84c4a02026
Pavol Kurina <pavol.kurina@gmail.com>
2023-11-06 16:58:21 +0100

    added dynamicresources.app

---
+++ dynamicresources.app/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace dynamicresources.app;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ dynamicresources.app/Views/MarkupExcensions.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Maui.Markup;
+using System.Globalization;
+
+namespace dynamicresources.app.Views
+{
+    public static class MarkupExtensions
+
+    {
+
+        public static Picker ItemsSource<T>(this Picker itemsView, IEnumerable<T> itemsSource)
+        {
+            itemsView.ItemsSource = itemsSource?.ToList() ?? Enumerable.Empty<T>().ToList();
+            itemsView.SelectedIndex = -1;
+            return itemsView;
+        }
+
+        public static Button BindAction(this Button b, string commandPath, object? source = null, Func<string>? textGenerator = null)
+        {
+            if (textGenerator != null) {
+                b.Bind(Button.TextProperty, "SelectedLocale", convert: (CultureInfo _) => textGenerator());
+            }
+            b.BindCommand(commandPath, source);
+            return b;
+        }
+
+        public static Label BindText(this Label l, Func<string> textGenerator)
+        {
+            l.Bind(Label.TextProperty, "SelectedLocale", convert: (CultureInfo _) => textGenerator());
+            return l;
+        }
+
+        // REVIEW: how to enforce the check that the viewmodel has "SelectedLocate" on compile-time
+
+        public static Button BindText(this Button b, Func<string> textGenerator)
+        {
+            b.Bind(Button.TextProperty, "SelectedLocale", convert: (CultureInfo _) => textGenerator());
+            return b;
+        }
+
+    }
+}
