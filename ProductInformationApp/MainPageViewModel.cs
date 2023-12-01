b6b107303cfde84f827293df632fdce177e32c6c
Pavol Kurina <pavol.kurina@gmail.com>
2023-12-01 05:08:49 +0100

    extracted ViewModel into file

---
+++ ProductInformationApp/MainPageViewModel.cs
@@ -0,0 +1,81 @@
+﻿using ProductInformationApp.Services;
+using Prins;
+using System.Collections.Immutable;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProductInformationApp
+{
+    partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Overview))]
+        [NotifyPropertyChangedFor(nameof(CommonInformation))]
+        [NotifyPropertyChangedFor(nameof(Hints))]
+        [NotifyPropertyChangedFor(nameof(TradingUnit))]
+        [NotifyPropertyChangedFor(nameof(UnitPack))]
+        [NotifyPropertyChangedFor(nameof(Palette))]
+        [NotifyPropertyChangedFor(nameof(Ingredients))]
+        [NotifyPropertyChangedFor(nameof(NutrientAnnotation))]
+        [NotifyPropertyChangedFor(nameof(Nutrients))]
+        [NotifyPropertyChangedFor(nameof(Additives))]
+        [NotifyPropertyChangedFor(nameof(Alergens))]
+        Product product;
+
+        public record KeyValue(string Key, object Value);
+        public record KeyValueUnit(string Key, object Value, string Unit);
+
+        public ImmutableList<KeyValue> Overview 
+        { 
+            get => Product.GetOverview().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList(); 
+        }
+
+        public ImmutableList<KeyValue> CommonInformation
+        { 
+            get => Product.GetCommonInformation().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList(); 
+        }
+
+        public ImmutableList<KeyValue> Hints
+        { 
+            get => Product.GetHints().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList(); 
+        }
+
+        public ImmutableList<KeyValue> TradingUnit
+        { 
+            get => Product.GetTradingUnit().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList();
+        }
+
+        public ImmutableList<KeyValue> UnitPack
+        { 
+            get => Product.GetUnitPack().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList();
+        }
+
+        public ImmutableList<KeyValue> Palette
+        { 
+            get => Product.GetPaletteInfo().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList();
+        }
+
+        public string Ingredients { get => Product?.Ingredients?.Value ?? "-"; }
+
+        public string NutrientAnnotation { get => Product?.NutrientAnnotation?.Value ?? "-"; }
+
+        public ImmutableList<KeyValueUnit> Nutrients 
+        { 
+            get => Product.GetNutrients().Select(o => new KeyValueUnit(o.Item1, o.Item2, o.Item3)).ToImmutableList();
+        }
+
+        public ImmutableList<KeyValue> Additives 
+        { 
+            get => Product.GetAdditives().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList();
+        }
+
+        public ImmutableList<KeyValue> Alergens 
+        { 
+            get => Product.GetAlergens().Select(o => new KeyValue(o.Item1, o.Item2)).ToImmutableList();
+        }
+
+        public MainPageViewModel(Product product)
+        {
+            Product = product;
+        }
+    }
+}
