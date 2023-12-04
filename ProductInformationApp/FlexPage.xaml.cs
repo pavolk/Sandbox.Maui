0fad58a760b1cbd360931f21aab9a84a8b3c25b2
Pavol Kurina <pavol.kurina@gmail.com>
2023-12-04 18:34:09 +0100

    some experimentes with flexlayout

---
+++ ProductInformationApp/FlexPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductInformationApp
+{
+    public partial class FlexPage : ContentPage
+    {
+        public record KeyValue(string Key, object Value);
+
+        public IEnumerable<KeyValue> Items { get; set; } = new List<KeyValue>();
+
+        public FlexPage()
+        {
+            InitializeComponent();
+            Items = new List<KeyValue>() {
+                { new KeyValue("Key1", "Value1") },
+                { new KeyValue("Key2", "Value2") },
+                { new KeyValue("Key3", "Value3") }
+
+            };
+
+            BindingContext = this;
+        }
+    }
+}
